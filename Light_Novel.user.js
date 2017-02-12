4b59c3ea4aab6b0667db091616578db0ada226c4
zecy <angelszc163@gmail.com>
2017-02-12 15:53:13 +0800

    Tampermonkey 脚本，用于调用主函数

---
+++ Light_Novel.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         Light_Novel
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  clean lk post
+// @run-at       document-end
+// @author       zecy
+// @match        *.lightnovel.cn/thread*
+// @require      file:///Users/zecy/www/personal-scripts/chrom_user_script/assets/js/jquery-3.1.1.js
+// @require      file:///Users/zecy/www/personal-scripts/chrom_user_script/light-novel/light-novel.js
+// ==/UserScript==
+
+main();

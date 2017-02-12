f8cbd2de53117863a2454ad8aac1fa4aa7774298
zecy <angelszc163@gmail.com>
2017-02-12 16:38:43 +0800

    修改为在「只看该作者」页面中使用

--- Light_Novel.user.js
+++ Light_Novel.user.js
@@ -5,7 +5,7 @@
 // @description  clean lk post
 // @run-at       document-end
 // @author       zecy
-// @match        *.lightnovel.cn/thread*
+// @match        *.lightnovel.cn/forum.php?mod=viewthread
 // @require      file:///Users/zecy/www/personal-scripts/chrom_user_script/assets/js/jquery-3.1.1.js
 // @require      file:///Users/zecy/www/personal-scripts/chrom_user_script/light-novel/light-novel.js
 // ==/UserScript==
